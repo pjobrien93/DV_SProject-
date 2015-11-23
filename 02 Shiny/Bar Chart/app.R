01cdac6f66d0e5da2a22114bd20a87d31b1380c5
Patricia O'Brien <patriciaobrien@wireless-10-145-254-156.public.utexas.edu>
2015-11-23 12:19:27 -0600

    no message

--- 02 Shiny/Bar Chart/app.R
+++ 02 Shiny/Bar Chart/app.R
@@ -12,19 +12,21 @@ library(shiny)
 
 ui <- fluidPage(
   # *Input() inputs
-  sliderInput( inputId = "instn", 
-               label = "Choose an Institution",
-               multiple=FALSE),
+#  sliderInput( inputId = "instn", 
+#               label = "Choose an Institution",
+#               value=25, min=1, max=100),
   
   # *Output() ouputs
   plotOutput(outputId = "bar")
 )
 
 server <- function(input, output) {
+  # data frame read in 
+  df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select INSTNM, LIQUOR12, DRUG12, WEAPON12 from RESIDENCEHALLARREST2013"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_pjo293', PASS='orcl_pjo293', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
+  
+  
   output$bar <- renderPlot({
     #blah multiple lines of code
-    df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select INSTNM, LIQUOR12, DRUG12, WEAPON12 from RESIDENCEHALLARREST2013"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_pjo293', PASS='orcl_pjo293', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-    
     df <- df %>% group_by(INSTNM) %>% filter(LIQUOR12 != 0, DRUG12 != 0, WEAPON12 !=0) %>% filter(LIQUOR12 != "null", DRUG12 != "null", WEAPON12 != "null")
     
     df$LIQUOR12 <- as.numeric(as.character(df$LIQUOR12))
@@ -34,7 +36,7 @@ server <- function(input, output) {
     df <- melt(df[,c('INSTNM','LIQUOR12','DRUG12', 'WEAPON12')],id.vars = "INSTNM")
     df$value <- as.numeric(as.character(df$value))
     
-    ggplot(data=df(), aes(input$instn, value)) +   
+    ggplot(df, aes(INSTNM, value)) +   
       geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
       coord_flip()
   })
