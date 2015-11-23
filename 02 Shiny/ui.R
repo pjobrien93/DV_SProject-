4b6facdf2219aa8516bf3b4712da81c5a22f4385
mohcho <mmchauhan11@gmail.com>
2015-11-23 09:42:18 -0600

    no message

---
+++ 02 Shiny/ui.R
@@ -0,0 +1,38 @@
+library(shiny)
+require(shinydashboard)
+require(leaflet)
+
+dashboardPage(
+  dashboardHeader(
+  ),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Scatter Plot", tabName = "scatter", icon = icon("dashboard")),
+      menuItem("Crosstab", tabName = "crosstab", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "scatter",
+              actionButton(inputId = "clicks1",  label = "Click me"),
+              plotOutput("scatterPlot")
+              
+      )
+  #    tabItem(tabName = "crosstab"),
+  #            sidebarPanel(
+  #              actionButton(inputId = "light", label = "Light"),
+  #              actionButton(inputId = "dark", label = "Dark"),
+  #              sliderInput("KPI1", "KPI_Low_Max_value:", 
+  #                          min = 0, max = 0.01,  value = 0.0018),
+  #              sliderInput("KPI2", "KPI_Medium_Max_value:", 
+  #                          min = 0.01, max = 0.02,  value = 0.01),
+  #              textInput(inputId = "title", 
+  #                        label = "Crosstab Title",
+  #                        value = "Crosstab"),
+  #              actionButton(inputId = "clicks2",  label = "Click me"),
+  #            ),
+  #           plotOutput("crosstab")
+    )
+  )
+)
+
